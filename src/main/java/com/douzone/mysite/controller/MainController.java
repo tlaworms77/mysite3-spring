3e2b86a990032e5f153e82e2c2661c60bc770332
tlawo <tlawo@LAPTOP-ICSS4D7B>
2019-02-19 12:56:33 +0900

    default-servlet handler 적용하여 절대경로로 설정된것을 인식시켜 css 적용 /localhost/mysite3 ... :D

---
+++ src/main/java/com/douzone/mysite/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.douzone.mysite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping({"", "/main"})
+	public String main() {
+		return "/WEB-INF/views/main/index.jsp";
+		//return "/main/index";
+	}
+	
+}
